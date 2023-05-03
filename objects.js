47aafb961c7f57f671391e7c349288ba6ba81ccd
codingvaleria <codingvaleria@gmail.com>
2023-05-03 18:08:05 +0300

    Deleting properties on objects

--- objects.js
+++ objects.js
@@ -1,76 +1,76 @@
-// //  Creating objects - They represent real entites
-// // Object literal
+//  Creating objects - They represent real entites
+// Object literal
 
-// let alien = {
-//   name: "Valeria",
-//   technology: "JavaScript",
-//   age: 20,
-//   laptop: {
-//     cpu: "i7",
-//     ram: 4,
-//     brand: "Asus",
-//   },
-// };
+let alien = {
+  name: "Valeria",
+  technology: "JavaScript",
+  age: 20,
+  laptop: {
+    cpu: "i7",
+    ram: 4,
+    brand: "Asus",
+  },
+};
 
-// console.log(alien);
-// // Accessing properites
-// // Use dot operation
-// console.log(alien.name);
-// console.log(alien.technology);
-// console.log(alien.age);
+console.log(alien);
+// Accessing properites
+// Use dot operation
+console.log(alien.name);
+console.log(alien.technology);
+console.log(alien.age);
 
-// // using bracket notation
-// console.log(alien["name"]);
-// console.log(alien["technology"]);
-// console.log(alien["age"]);
+// using bracket notation
+console.log(alien["name"]);
+console.log(alien["technology"]);
+console.log(alien["age"]);
 
-// console.log(alien.laptop);
-// console.log(alien.laptop.brand.length);
+console.log(alien.laptop);
+console.log(alien.laptop.brand.length);
 
-// // delete
-// console.log(delete alien.laptop);
-// console.log(alien);
+// delete
+console.log(delete alien.laptop);
+console.log(alien);
 
-// // print properties of an object one by one
+// print properties of an object one by one
 
-// for (let key in alien) {
-//   console.log(key);
-//   console.log(key, alien[key]); // fetch value
-// }
+for (let key in alien) {
+  console.log(key);
+  console.log(key, alien[key]); // fetch value
+}
 
-// // Fetch all properties of laptop
-// for (let key in alien.laptop) {
-//   console.log(alien.laptop[key]);
-// }
+// Fetch all properties of laptop
+for (let key in alien.laptop) {
+  console.log(alien.laptop[key]);
+}
 
-// //  Looping over over Objects
-// let user = {
-//   name: 'John',
-//   age: 20,
-//   occupation : 'Teacher',
-// }
-// // Looping over to get property names
-// let propertyNames = Object.keys(user);
-// for (let propertyName of propertyNames) {
-//   console.log(propertyName);
-// }
+//  Looping over over Objects
+let user = {
+  name: 'John',
+  age: 20,
+  occupation : 'Teacher',
+}
+// Looping over to get property names
+let propertyNames = Object.keys(user);
+for (let propertyName of propertyNames) {
+  console.log(propertyName);
+}
 
-// //  Looping over to get property values
-// let propertyValues = Object.values(user);
-// for (let propertyValue of propertyValues) {
-//   console.log(propertyValue);
-// }
+//  Looping over to get property values
+let propertyValues = Object.values(user);
+for (let propertyValue of propertyValues) {
+  console.log(propertyValue);
+}
 
-// // Looping over to get both values and keys
-// let entries = Object.entries(user);
-// for (let entry of entries) {
-//   console.log(`${entry[0]}: ${entry[1]}`);
-// }
+// Looping over to get both values and keys
+let entries = Object.entries(user);
+for (let entry of entries) {
+  console.log(`${entry[0]}: ${entry[1]}`);
+}
 
-// // or
-// for (let [key, val] of entries) {
-//   console.log(`${key}: ${val}`);
-// }
+// or
+for (let [key, val] of entries) {
+  console.log(`${key}: ${val}`);
+}
 
 // Non destructively update objects
 
@@ -100,4 +100,15 @@ function nondestructivelyUpdateObject(obj, key, value) {
   };
 }
 
+// Remove properties from an object
+let cat = {
+  name: "Rose",
+  age: 6,
+  color: "white",
+  owner: "Eeeks",
+}
 
+console.log(delete cat.owner);
+// => true
+console.log(cat)
+// => { name: 'Rose', age: 6, color: 'white' }
