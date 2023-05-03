cc2b806545f0b1a4e4d3bd79356f7d0c6e78a587
codingvaleria <codingvaleria@gmail.com>
2023-05-03 11:49:33 +0300

    Create an object solution

---
+++ objectquizes.js
@@ -0,0 +1,21 @@
+// Problem 1: Create an Object
+//  *
+//  * Description:
+//  * Write a JavaScript function that creates an object called `person` with three properties:
+//  * `name`, `age`, and `email`. The values of these properties should be your own name, age,
+//  * and email address, respectively. Return the `person` object. Call the function and log
+//  * the resulting object to the console.
+//  */
+
+function createPersonObject() {
+  // TODO: Write your solution here
+  let person = {
+    name: "Valeria",
+    age: 16,
+    email: "valeria.email@example.com",
+  };
+  return person;
+}
+
+console.log(createPersonObject()); // should log { name: "Your Name", age: 20, email: "your.email@example.com"}
+
