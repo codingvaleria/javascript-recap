ef4c18fdb1aa1975d929dcdf5b568e9aa9fc244c
codingvaleria <codingvaleria@gmail.com>
2023-05-06 16:14:29 +0300

    more refactoring -functions

--- votingapp.js
+++ votingapp.js
@@ -1,48 +1,45 @@
-const prompt = require("prompt-sync")();
-let currentPassword = "abcd";
-let arrayOfCandidates = ["Alex", "Jane", "Alice", "Mark"];
-
 // Problem: Simple Voting Command Line Application
 
 // Description:
 // Create a command line application using JavaScript that allows an admin to add candidates, a voter to view a list of the candidates and vote for their favorite candidate, and the admin to display the score of each candidate. The application should be in a loop and only the admin should be able to stop it. The admin should have a password and they should be able to reset it.
 
-// Requirements:
+const prompt = require("prompt-sync")();
 
-// 1. Display a welcome message to the user when the application is started.
-const welcomeMessage = '     Welcome to Kura yako voting app    '
-console.log(welcomeMessage);
+let savedPassword = "abcd";
+let arrayOfCandidates = ["Alex", "Jane", "Alice", "Mark"];
+let votesCount = [0, 0, 0, 0];
 
- console.log(
-  `Please select choice to login:
-    1. As an admin
-    2. As a voter
-    `
- );
-
-let choice = prompt('>')
-
-// 2. Implement a login feature for the admin using a password.
-
-if (choice === "1") {
-  console.log(
-    "                WELCOME TO THE ADMIN PORTAL                     "
-  );
-
+function checkPassword() {
   let passwordPrompts = 1;
   for (passwordPrompts; passwordPrompts <= 3; passwordPrompts++) {
     inputPassword = prompt("Enter your password: ");
-    if (inputPassword !== currentPassword) {
-      console.log("Login unsuccessful, input password again");
+    if (inputPassword !== savedPassword) {
+      console.log("Invalid password try again");
     } else {
       break;
     }
   }
   if (passwordPrompts > 3) {
-    console.log("Your account is blocked consult your admin");
+    console.log("Your account is locked, contact admin");
+    return false;
+  } else {
+    return true;
   }
-//   3. Once the admin is logged in, display a menu of options that the admin can choose from.
-  else {
+}
+
+function printCandidates() {
+  let i = 1;
+  console.log("The list of candidates is:");
+  while (i <= arrayOfCandidates.length) {
+    console.log(`${i}. ${arrayOfCandidates[i - 1]}`);
+    i++;
+  }
+}
+
+function adminLogic() {
+  const isPasswordCorrect = checkPassword();
+  //   3. Once the admin is logged in, display a menu of options that the admin can choose from.
+  if (isPasswordCorrect === true) {
     console.log(`
     login successful
     Here's a list of actions you can take: 
@@ -50,93 +47,92 @@ if (choice === "1") {
         2. Display score of each candidate
         3. Reset your password
     `);
-    
-     let action = prompt(
-          `Please select actions you would like to perform :`     
-     );
+    let action = prompt(`Please select actions you would like to perform :`);
     if (action === "1") {
       console.log(`Here's a list of existing candidates`);
       // listOfCandidates)
-
-// 4. Implement the ability for the admin to add new candidates to the list of candidates.
+      // 4. Implement the ability for the admin to add new candidates to the list of candidates.
       let newCandidate = prompt(
         "Please key in the name of candidate you would like to add:"
       );
       arrayOfCandidates.push(newCandidate);
-      let i = 1;
-      console.log("The updated list of candidates is:");
-      while (i <= arrayOfCandidates.length) {
-        console.log(`${i}. ${arrayOfCandidates[i - 1]}`);
-        i++;
-      }
+      printCandidates();
     }
- 
+    // 5. Implement the ability for the admin to reset the password.
+    else if (action === "3") {
+      // inputPassword = prompt("Provide your old password:");
+      console.log("Please provide your old password in the next prompt");
+      const isPasswordCorrect = checkPassword();
+      if (isPasswordCorrect === true) {
+        newPassword = prompt("Set your new password: ");
+        savedPassword = newPassword;
+        console.log("Password changed successfully");
+      }
+      // console.log(savedPassword);
+    }
   }
-}      
-//       // 5. Implement the ability for the admin to reset the password.
-//       else if (action === "3") {
-//         newPassword = prompt("Set your new password: ");
-//         currentPassword = newPassword;
-//         console.log("Password changed successfully");
-//         // console.log(currentPassword);
-//       }
-//     }
-//     // console.log("Your account is blocked consult your admin");
-// }
-// else if (choice === "2") {
-// // 6. Implement the ability for a voter to view the list of candidates.
-//   console.log(
-//     "             WELCOME TO THE KURA YAKO VOTING PORTAL                     "
-//   );
+}
 
-//     let i = 1;
-//     console.log("Here's a list of vying candidates:")
-//     function candidateList() {
-//         while (i <= arrayOfCandidates.length) {
-//         console.log(`${i}. ${arrayOfCandidates[i]}`);
-//           i++;
-//         }
-//     }
-//     candidateList();
-// }
-// else {
-//     console.log("Enter a valid choice")
-// }
-// // 7. Implement the ability for a voter to vote for their favorite candidate.
-// let vote =
-//   prompt(` To vote for a candidate, key in their corresponding number for a vote,
-// What candidate are you voting for? `);
+function voterLogic() {
+  console.log(
+    "             WELCOME TO THE KURA YAKO VOTING PORTAL                     "
+  );
+  printCandidates();
+  // 7. Implement the ability for a voter to vote for their favorite candidate.
+  console.log(` To vote for a candidate, key in their corresponding number for a vote,
+What candidate are you voting for? `);
+  let vote = prompt("> ");
+  let voteNumber = parseInt(vote);
+  if (
+    voteNumber != NaN &&
+    voteNumber > 0 &&
+    voteNumber <= arrayOfCandidates.length
+  ) {
+    votesCount[voteNumber - 1]++;
+  }
+  console.log(votesCount);
+}
 
-// let janeCount = 0;
-// let aliceCount = 0;
-// let markCount = 0;
+while (true) {
+  // 1. Display a welcome message to the user when the application is started.
+  const welcomeMessage = "     Welcome to Kura yako voting app    ";
+  console.log(welcomeMessage);
 
-// function votesCount() {
-//   if (vote === "1") {
-//     janeCount = janeCount++;
-//     console.log("you've voted for Jane");
-//   } else if (vote === "2") {
-//     aliceCount = aliceCount++;
-//     console.log("you've voted for Alice");
-//   } else if (vote === "3") {
-//     markCount = markCount++;
-//     console.log("you've voted for Mark");
-//   } else {
-//     console.log("please vote for a contesting candidate");
-//   }
-//  }
+  console.log(
+    `Please select choice to login:
+    1. As an admin
+    2. As a voter
+    `
+  );
 
-// // 8. Implement the ability for the admin to display the score of each candidate.
-// console.log(`
-// Jane: ${janeCount}
-// Alice: ${aliceCount}
-// Mark: ${markCount}
-// `)
-// // 9. Implement a loop that keeps the application running until the admin chooses to stop it.
+  let choice = prompt("> ");
 
-// // 10. Add appropriate error handling to handle incorrect input and prevent the application from crashing.
-// // 11. Implement a command that allows the admin to exit the loop and stop the application.
-// // 12. Display a farewell message to the user when the application is stopped.
+  // 2. Implement a login feature for the admin using a password.
+  if (choice === "1") {
+    console.log(
+      "                WELCOME TO THE ADMIN PORTAL                     "
+    );
+    adminLogic();
+  } else if (choice === "2") {
+    // 6. Implement the ability for a voter to view the list of candidates.
 
-// // Note: You can choose to implement the requirements in any order you prefer.
-// //       consider using "prompt-sync" for getting the user's input
+    voterLogic();
+  } else {
+    console.log("Enter a valid choice");
+  }
+
+  // // 8. Implement the ability for the admin to display the score of each candidate.
+  // console.log(`
+  // Jane: ${janeCount}
+  // Alice: ${aliceCount}
+  // Mark: ${markCount}
+  // `)
+  // // 9. Implement a loop that keeps the application running until the admin chooses to stop it.
+
+  // // 10. Add appropriate error handling to handle incorrect input and prevent the application from crashing.
+  // // 11. Implement a command that allows the admin to exit the loop and stop the application.
+  // // 12. Display a farewell message to the user when the application is stopped.
+
+  // // Note: You can choose to implement the requirements in any order you prefer.
+  // //       consider using "prompt-sync" for getting the user's input
+}
