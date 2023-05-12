3281e06c46d880250b4556a92650a1e77f41795e
codingvaleria <codingvaleria@gmail.com>
2023-05-13 02:12:18 +0300

    Array Conversion, array intersection, chunking and array to object conversion solutions

--- arrayandobjectsquizes.js
+++ arrayandobjectsquizes.js
@@ -18,6 +18,19 @@ function objectToArray(obj) {
 console.log(objectToArray({ name: "John", age: 25, city: "New York" }));
 // should log [["name", "John"], ["age", 25], ["city", "New York"]]
 
+function objectToArray2(obj) {
+  let objectKeys = Object.keys(obj);
+  let objectValues = Object.values(obj);
+  let keyValueArray = [];
+
+  for (let i = 0; i < objectKeys.length && i < objectValues.length; i++) {
+    keyValueArray.push([objectKeys[i], objectValues[i]]);
+  }
+  return keyValueArray;
+}
+console.log(objectToArray2({ name: "John", age: 25, city: "New York" }));
+// should log [["name", "John"], ["age", 25], ["city", "New York"]]
+
 // Problem 2: Array Intersection
 //  *
 //  * Description:
@@ -40,9 +53,29 @@ function arrayIntersection(arr1, arr2) {
   return intersection;
 }
 
-console.log(arrayIntersection([1, 2, 3], [2, 3, 4])); // should log [2, 3]
+// console.log(arrayIntersection([1, 2, 3], [2, 3, 4])); // should log [2, 3]
+// console.log(
+//   arrayIntersection(["apple", "banana", "orange"], ["banana", "pear"])
+// ); // should log ["banana"]
+
+function arrayIntersection2(arr1, arr2) {
+  // TODO: Write your solution here
+  let intersection = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let elOne = arr1[i];
+    for (let j = 0; j < arr2.length; j++) {
+      if (elOne === arr2[j]) {
+        intersection.push(arr2[j]);
+      }
+    }
+  }
+  return intersection;
+}
+
+console.log(arrayIntersection2([1, 2, 3], [2, 3, 4])); // should log [2, 3]
 console.log(
-  arrayIntersection(["apple", "banana", "orange"], ["banana", "pear"])
+  arrayIntersection2(["apple", "banana", "orange"], ["banana", "pear"])
 ); // should log ["banana"]
 
 //  Problem 3: Object Difference
@@ -66,6 +99,52 @@ function objectDifference(obj1, obj2) {
   }
   return uniqueproperties;
 }
-
 console.log(objectDifference({ a: 1, b: 2, c: 3 }, { b: 2 })); // should log {a: 1, c: 3}
 console.log(objectDifference({ x: "foo", y: "bar" }, { y: "baz", z: "qux" })); // should log {x: "foo"}
+
+// Problem: Array Chunking
+//  *
+//  * Description:
+//  * Write a JavaScript function that takes in an array and a chunk size and returns a new array
+//  * where each element of the original array is split into subarrays of length chunk size (except
+//  * for the last element, which can be shorter). For example, if the input array is [1, 2, 3, 4, 5]
+//  * and the chunk size is 2, the function should return [[1, 2], [3, 4], [5]]. Call the function
+//  * with an array of your choice and a chunk size of your choice and log the result to the console.
+//  */
+
+function arrayChunking(arr, chunkSize) {
+  //   // TODO: Write your solution here
+  let chunkedArray = [];
+  let chunks = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (chunks.length < chunkSize) {
+      chunks.push(arr[i]);
+    }
+    if (chunks.length === chunkSize) {
+      chunkedArray.push(chunks);
+      chunks = [];
+    }
+  }
+    if (chunks.length != 0) {
+      chunkedArray.push(chunks);
+    }
+  return chunkedArray;
+}
+
+console.log(arrayChunking([1, 2, 3, 4, 5], 2)); // should log [[1, 2], [3, 4], [5]]
+console.log(arrayChunking(["apple", "banana", "orange", "pear", "grape"], 3)); // should log [["apple", "banana", "orange"], ["pear", "grape"]]
+
+// Problem: Array to object conversion
+// Convert this array into an object of key value pairs
+let array = [
+  ["name", "John"],
+  ["age", 25],
+  ["city", "New York"],
+];
+
+let object = {}
+for (let i = 0; i < array.length; i++) {
+  object[array[i][0]] = array[i][1];
+}
+console.log(object);
