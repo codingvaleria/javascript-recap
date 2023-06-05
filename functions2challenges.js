1a8818f22ad1a77682d160cb68740723584cd9b5
codingvaleria <codingvaleria@gmail.com>
2023-06-05 16:17:34 +0300

    Grade analytics solution

--- functions2challenges.js
+++ functions2challenges.js
@@ -59,3 +59,94 @@ console.log(averageGrades);
 //   { name: "Alice", averageGrade: 80 },
 //   { name: "Bob", averageGrade: 92.33333333333333 }
 // ]
+
+// Problem: Grade Analytics
+//  *
+//  * Description:
+//  * Write a JavaScript program that performs various analytics on a set of student grades. The program should include the following functions:
+//  *
+//  * 1. `calculateAverageGrade(grades)`: Calculates the average grade from an array of grades.
+//  * 2. `findHighestGrade(grades)`: Finds the highest grade from an array of grades.
+//  * 3. `findLowestGrade(grades)`: Finds the lowest grade from an array of grades.
+//  * 4. `gradeAnalytics(students)`: Takes in an array of student objects. Each student object has the
+//  *         properties `name` (string) and `grades` (array of numbers). The function should calculate and
+//  *         return an object with the following analytics:
+//  *    - `averageGrade`: The average grade across all students.
+//  *    - `highestGrade`: The highest grade among all students.
+//  *    - `lowestGrade`: The lowest grade among all students.
+//  *
+//  * Implement the functions and the `gradeAnalytics` function as described above.
+//  */
+
+function calculateAverageGrade2(grades) {
+  // TODO: Write your solution here
+  let sumofGrades2 = 0;
+  for (let i = 0; i < grades.length; i++) {
+    sumofGrades2 = sumofGrades2 + grades[i];
+  }
+  let average = sumofGrades2 / grades.length;
+  return average;
+}
+
+function findHighestGrade(grades) {
+  // TODO: Write your solution here
+  let highestGrade = grades[0];
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > highestGrade) {
+      highestGrade = grades[i];
+    }
+  }
+  return highestGrade;
+}
+
+function findLowestGrade(grades) {
+  // TODO: Write your solution here
+  let lowestGrade = grades[0];
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] < lowestGrade) {
+      lowestGrade = grades[i];
+    }
+  }
+  return lowestGrade;
+}
+
+function gradeAnalytics(students2) {
+  // TODO: Write your solution here
+  let nestedgrades = [];
+  for (let i = 0; i < students2.length; i++) {
+    nestedgrades.push(students2[i].grades);
+  }
+  let gradesArray = [].concat(...nestedgrades);
+  console.log(gradesArray);
+  let results = {
+    averageGrade: calculateAverageGrade2(gradesArray),
+    highestGrade: findHighestGrade(gradesArray),
+    lowestGrade: findLowestGrade(gradesArray),
+  };
+  return results;
+}
+
+const students2 = [
+  {
+    name: "John",
+    grades: [90, 85, 95],
+  },
+  {
+    name: "Alice",
+    grades: [80, 75, 85],
+  },
+  {
+    name: "Bob",
+    grades: [95, 90, 92],
+  },
+];
+
+const analytics = gradeAnalytics(students2);
+
+console.log(analytics);
+// should log:
+// {
+//   averageGrade: 88.66666666666667,
+//   highestGrade: 95,
+//   lowestGrade: 75
+// }
