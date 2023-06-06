9819928eb3d13027c926d91bc59f846613fb9382
codingvaleria <codingvaleria@gmail.com>
2023-06-06 18:24:31 +0300

    Practice creating classes

--- classes.js
+++ classes.js
@@ -92,3 +92,28 @@ class Transaction3 {
 }
 const transaction3 = new Transaction3(100.24, "03/04/2018", "Grocery Shopping");
 transaction3.amount; // undefined;
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+}
