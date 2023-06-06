51157c494573726d2fea63566d157af960497f10
codingvaleria <codingvaleria@gmail.com>
2023-06-06 18:46:37 +0300

    More on classes practise

--- classes.js
+++ classes.js
@@ -117,3 +117,36 @@ class Dinner {
     this.#dessert = dessert;
   }
 }
+
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says meow`;
+  }
+}
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    if (this.sex === "male") {
+      return `It's me ! ${this.name} the parrot`;
+    } else {
+      return `${this.name} says sqawk!`;
+    }
+  }
+}
