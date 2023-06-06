07b7c1353f55d02dde55b9d304fdad193ca56f59
codingvaleria <codingvaleria@gmail.com>
2023-06-06 18:03:32 +0300

    Private class methods

--- classes.js
+++ classes.js
@@ -46,3 +46,49 @@ const square1 = new Square(5);
 square1; // Square { sideLength: 5 }
 square1.area(); // 25
 
+// Private Properties
+// in normal situations, properties are accessible outside the class
+class Transaction {
+  constructor(amount, date, memo) {
+    this.amount = amount;
+    this.date = date;
+    this.memo = memo;
+  }
+}
+
+const deposit = new Transaction(100.24, "06/06/2023", "Grocery store");
+deposit.amount; //100.24
+deposit.amount = 10000.24;
+deposit.amount; // 10000.24;
+
+// to make methods and properties private, prepend #
+// They cannot be accessed or changed from outside the class.
+class Transaction2 {
+  // declare private fields
+  #amount;
+  #date;
+  #memo;
+  constructor(amount, date, memo) {
+    // assign values to private fields
+    this.#amount = amount;
+    this.#date = date;
+    this.#memo = memo;
+  }
+}
+const transaction2 = new Transaction2(100.24, "03/04/2018", "Grocery Shopping");
+transaction2.amount; // undefined;
+
+// Confirm  if i have some methods private
+class Transaction3 {
+  // declare private fields
+  #amount;
+
+  constructor(amount, date, memo) {
+    // assign values to private fields
+    this.#amount = amount;
+    this.date = date;
+    this.memo = memo;
+  }
+}
+const transaction3 = new Transaction3(100.24, "03/04/2018", "Grocery Shopping");
+transaction3.amount; // undefined;
