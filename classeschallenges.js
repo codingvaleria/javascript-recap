d7c5cae6012b9a11d081bf753664839a78818c7b
codingvaleria <codingvaleria@gmail.com>
2023-06-09 14:42:48 +0300

    Rectangle class solution

--- classeschallenges.js
+++ classeschallenges.js
@@ -42,3 +42,47 @@ class Book {
 
 const book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
 console.log(book.getInfo()); // should log "The Great Gatsby, F. Scott Fitzgerald, 1925"
+
+// Problem: Rectangle Class
+//  *
+//  * Description:
+//  * Create a Rectangle class that represents a rectangle. The class should have the following properties:
+//  * - width: the width of the rectangle
+//  * - height: the height of the rectangle
+//  *
+//  * The class should also have the following methods:
+//  * - getWidth(): returns the width of the rectangle
+//  * - getHeight(): returns the height of the rectangle
+//  * - getArea(): returns the area of the rectangle (width * height)
+//  * - getPerimeter(): returns the perimeter of the rectangle (2 * (width + height))
+//  *
+//  * Create an instance of the Rectangle class with width 5 and height 10. Call the getArea() and getPerimeter() methods to log the calculated values to the console.
+//  */
+
+class Rectangle {
+  // TODO: Implement the Rectangle class
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+
+  getPerimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
+
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.getArea()); // should log 50
+console.log(rectangle.getPerimeter()); // should log 30
