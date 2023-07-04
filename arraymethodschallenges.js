120597bad6bdecda00f3c23eb14206b5af9296c1
codingvaleria <codingvaleria@gmail.com>
2023-07-04 19:58:31 +0300

    Uncomment code

--- arraymethodschallenges.js
+++ arraymethodschallenges.js
@@ -8,16 +8,16 @@
  * with an array of your choice and log the result to the console.
  */
 
-// function convertCelsiusToFahrenheit(celsiusTemperatures) {
-//   // TODO: Write your solution here
-//   return celsiusTemperatures.map(
-//     (celsiusTemperature) => (celsiusTemperature * 9) / 5 + 32
-//   );
-// }
+function convertCelsiusToFahrenheit(celsiusTemperatures) {
+  // TODO: Write your solution here
+  return celsiusTemperatures.map(
+    (celsiusTemperature) => (celsiusTemperature * 9) / 5 + 32
+  );
+}
 
-// console.log(convertCelsiusToFahrenheit([0, 10, 25, 30])); // should log [32, 50, 77, 86]
-// console.log(convertCelsiusToFahrenheit([-40, 0, 100])); // should log [-40, 32, 212]
-// console.log(convertCelsiusToFahrenheit([15, 20, 35, 40])); // should log [59, 68, 95, 104]
+console.log(convertCelsiusToFahrenheit([0, 10, 25, 30])); // should log [32, 50, 77, 86]
+console.log(convertCelsiusToFahrenheit([-40, 0, 100])); // should log [-40, 32, 212]
+console.log(convertCelsiusToFahrenheit([15, 20, 35, 40])); // should log [59, 68, 95, 104]
 
 /*Problem: Filter Long Words
  *
@@ -30,16 +30,16 @@
  * `maxLength` of your choice and log the result to the console.
  */
 
-// function filterLongWords(words, maxLength) {
-//   // TODO: Write your solution here
-//   return words.filter((word) => word.length <= maxLength);
-// }
+function filterLongWords(words, maxLength) {
+  // TODO: Write your solution here
+  return words.filter((word) => word.length <= maxLength);
+}
 
-// console.log(filterLongWords(["apple", "banana", "grape", "kiwi"], 5)); // should log ["apple", "grape", "kiwi"]
-// console.log(
-//   filterLongWords(["programming", "javascript", "code", "developer"], 7)
-// ); // should log ["code"]
-// console.log(filterLongWords(["hello", "world"], 10)); // should log ["hello", "world"]
+console.log(filterLongWords(["apple", "banana", "grape", "kiwi"], 5)); // should log ["apple", "grape", "kiwi"]
+console.log(
+  filterLongWords(["programming", "javascript", "code", "developer"], 7)
+); // should log ["code"]
+console.log(filterLongWords(["hello", "world"], 10)); // should log ["hello", "world"]
 
 /**
  * Date: 2023-07-03
@@ -54,14 +54,14 @@
  * to the console.
  */
 
-// function checkPassFailStatus(scores, passingThreshold) {
-//   // TODO: Write your solution here
-//   return scores.every((score) => score > passingThreshold);
-// }
+function checkPassFailStatus(scores, passingThreshold) {
+  // TODO: Write your solution here
+  return scores.every((score) => score > passingThreshold);
+}
 
-// console.log(checkPassFailStatus([80, 90, 75, 85], 70)); // should log true
-// console.log(checkPassFailStatus([60, 55, 70, 65], 65)); // should log false
-// console.log(checkPassFailStatus([90, 95, 88, 92], 90)); // should log false
+console.log(checkPassFailStatus([80, 90, 75, 85], 70)); // should log true
+console.log(checkPassFailStatus([60, 55, 70, 65], 65)); // should log false
+console.log(checkPassFailStatus([90, 95, 88, 92], 90)); // should log false
 
 /* Problem: Check Array Inclusion
  *
@@ -73,16 +73,16 @@
  * with two arrays of your choice and log the result to the console.
  */
 
-// function checkArrayInclusion(mainArray, subArray) {
-//   // TODO: Write your solution here
-//   return mainArray.some((e) => subArray.includes(e));
-// }
+function checkArrayInclusion(mainArray, subArray) {
+  // TODO: Write your solution here
+  return mainArray.some((e) => subArray.includes(e));
+}
 
-// console.log(checkArrayInclusion([1, 2, 3, 4, 5], [2, 3])); // should log true
-// console.log(checkArrayInclusion([1, 2, 3, 4, 5], [6, 7])); // should log false
-// console.log(
-//   checkArrayInclusion(["apple", "banana", "orange"], ["banana", "orange"])
-// ); // should log true
+console.log(checkArrayInclusion([1, 2, 3, 4, 5], [2, 3])); // should log true
+console.log(checkArrayInclusion([1, 2, 3, 4, 5], [6, 7])); // should log false
+console.log(
+  checkArrayInclusion(["apple", "banana", "orange"], ["banana", "orange"])
+); // should log true
 
 /* Problem: Check Even Number Presence
  *
@@ -92,14 +92,14 @@
  * an array of your choice and log the result to the console.
  */
 
-// function checkEvenNumberPresence(numbers) {
-//   // TODO: Write your solution here
-//   return numbers.some((number) => number % 2 === 0);
-// }
+function checkEvenNumberPresence(numbers) {
+  // TODO: Write your solution here
+  return numbers.some((number) => number % 2 === 0);
+}
 
-// console.log(checkEvenNumberPresence([1, 3, 5, 7, 9])); // should log false
-// console.log(checkEvenNumberPresence([1, 2, 3, 4, 5])); // should log true
-// console.log(checkEvenNumberPresence([2, 4, 6, 8, 10])); // should log true
+console.log(checkEvenNumberPresence([1, 3, 5, 7, 9])); // should log false
+console.log(checkEvenNumberPresence([1, 2, 3, 4, 5])); // should log true
+console.log(checkEvenNumberPresence([2, 4, 6, 8, 10])); // should log true
 
 /*Problem: Calculate Total Value
  *
@@ -110,20 +110,20 @@
  * an object of your choice and log the result to the console.
  */
 
-// function calculateTotalValue(items) {
-//   // TODO: Write your solution here
-//   const itemValues = Object.values(items);
-//   let totalValue = itemValues.reduce((acc, value) => acc + value);
-//   return totalValue;
-// }
+function calculateTotalValue(items) {
+  // TODO: Write your solution here
+  const itemValues = Object.values(items);
+  let totalValue = itemValues.reduce((acc, value) => acc + value);
+  return totalValue;
+}
 
-// const items = {
-//   apple: 0.5,
-//   banana: 0.3,
-//   orange: 0.7,
-// };
+const items = {
+  apple: 0.5,
+  banana: 0.3,
+  orange: 0.7,
+};
 
-// console.log(calculateTotalValue(items)); // should log 1.5
+console.log(calculateTotalValue(items)); // should log 1.5
 
 /* Problem: Calculate Average
  *
@@ -135,15 +135,15 @@
  * the console.
  */
 
-// function calculateAverage(numbers) {
-//   // TODO: Write your solution here
-//     let sum = numbers.reduce((acc, number) => acc + number);
-//     return sum / numbers.length;
-// }
+function calculateAverage(numbers) {
+  // TODO: Write your solution here
+  let sum = numbers.reduce((acc, number) => acc + number);
+  return sum / numbers.length;
+}
 
-// console.log(calculateAverage([1, 2, 3, 4, 5])); // should log 3
-// console.log(calculateAverage([10, 20, 30, 40, 50])); // should log 30
-// console.log(calculateAverage([2, 4, 6, 8, 10])); // should log 6
+console.log(calculateAverage([1, 2, 3, 4, 5])); // should log 3
+console.log(calculateAverage([10, 20, 30, 40, 50])); // should log 30
+console.log(calculateAverage([2, 4, 6, 8, 10])); // should log 6
 
 /* Problem: Sum of Even Numbers
  *
@@ -154,21 +154,21 @@
  * with an array of your choice and log the result to the console.
  */
 
-// function sumOfEvenNumbers(numbers) {
-//   // TODO: Write your solution here
-//   const evenNumbers = [];
-//   numbers.map((number) => {
-//     if (number % 2 === 0) {
-//       evenNumbers.push(number);
-//     }
-//   });
+function sumOfEvenNumbers(numbers) {
+  // TODO: Write your solution here
+  const evenNumbers = [];
+  numbers.map((number) => {
+    if (number % 2 === 0) {
+      evenNumbers.push(number);
+    }
+  });
 
-//   return evenNumbers.reduce((acc, num) => acc + num, 0);
-// }
+  return evenNumbers.reduce((acc, num) => acc + num, 0);
+}
 
-// console.log(sumOfEvenNumbers([1, 2, 3, 4, 5, 6])); // should log 12
-// console.log(sumOfEvenNumbers([2, 4, 6, 8, 10])); // should log 30
-// console.log(sumOfEvenNumbers([1, 3, 5, 7, 9])); // should log 0
+console.log(sumOfEvenNumbers([1, 2, 3, 4, 5, 6])); // should log 12
+console.log(sumOfEvenNumbers([2, 4, 6, 8, 10])); // should log 30
+console.log(sumOfEvenNumbers([1, 3, 5, 7, 9])); // should log 0
 
 /* Problem: Group Students by Total Score
  *
